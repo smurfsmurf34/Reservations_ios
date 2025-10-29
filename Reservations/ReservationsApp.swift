fc5cb5704ef1998184998dc4ee11cec86b091a98
ioannisMBP14 <ioannis@paranikas.com>
2025-10-29 17:45:13 -0400

    Initial Commit

---
+++ Reservations/ReservationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationsApp.swift
+//  Reservations
+//
+//  Created by Ioannis on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ReservationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
